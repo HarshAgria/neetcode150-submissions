5e9c6357389fb119fd607944c3ff3a1cb012a9a7
Harsh Agria <147976668+HarshAgria@users.noreply.github.com>
2026-07-30 01:01:42 +0530

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.cpp
@@ -0,0 +1,15 @@
+class KthLargest {
+public:
+    int K;
+    vector<int> x;
+    KthLargest(int k, vector<int>& nums) {
+        K=k;
+        x=nums;
+    }
+    
+    int add(int val) {
+        x.push_back(val);
+        sort(x.begin(),x.end());
+        return x[x.size()-K];
+    }
+};
