bca592002dbc49c15760446488c57f3624ded5a1
Harsh Agria <147976668+HarshAgria@users.noreply.github.com>
2026-08-03 15:05:19 +0530

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        // BUcket SORT
+        unordered_map<int,int> m;
+        vector<int> ans;
+
+        for(int i:nums){
+            m[i]++;
+        }
+
+        vector<vector<int>> bucket(nums.size()+1);
+        for(auto i:m){
+            bucket[i.second].push_back(i.first);
+        }
+        for(int i=bucket.size()-1;i>=0 ; i--){
+
+            for(int i: bucket[i]){
+                if(ans.size()==k) break;
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+
+
+
+
+
+};
