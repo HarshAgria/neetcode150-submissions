9c798908c726ac0d460123f70542442964882614
Harsh Agria <147976668+HarshAgria@users.noreply.github.com>
2026-08-05 01:43:54 +0530

    Add: reconstruct-flight-path - submission-1

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-1.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<string> ans;
+
+    void traverse(string &s,unordered_map<string,priority_queue<string,vector<string>, greater<string>>> &m){
+
+        while(!m[s].empty()){
+            string x=m[s].top();
+            m[s].pop();
+            traverse(x,m);
+        }
+        ans.push_back(s);
+    }
+
+
+    vector<string> findItinerary(vector<vector<string>>& tickets){
+        // Appears simple but actually not
+        /*
+        think of like graph in which we need to traverse all edges
+        in lexilogically accending order.
+        the root node is 'JFK' and then we go on
+
+        the main idea is thinking of using priority queue in making adjacency list
+        such that unordered_map<string,priority_queue<string,vector<string>, greater<string>>>
+        */
+
+        unordered_map<string,priority_queue<string,vector<string>, greater<string>>> m;
+        for(auto &i:tickets){
+            m[i[0]].push(i[1]);
+        }
+        string root="JFK";
+        traverse(root,m);
+
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
